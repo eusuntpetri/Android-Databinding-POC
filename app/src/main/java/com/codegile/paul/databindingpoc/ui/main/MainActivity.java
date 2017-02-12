1bbe4290a2fd9980b982c57f63db06d473fb9f6f
Paul Hristea <paul.hristea@codegile.com>
2017-02-12 18:01:17 +0200

    Minor databinding optimizations.

--- app/src/main/java/com/codegile/paul/databindingpoc/ui/main/MainActivity.java
+++ app/src/main/java/com/codegile/paul/databindingpoc/ui/main/MainActivity.java
@@ -1,24 +1,19 @@
 package com.codegile.paul.databindingpoc.ui.main;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 
 import com.codegile.paul.databindingpoc.R;
-import com.codegile.paul.databindingpoc.ui.main.views.first.MainActivityFirstView;
+import com.codegile.paul.databindingpoc.databinding.MainActivityBinding;
 import com.codegile.paul.databindingpoc.ui.main.views.first.MainActivityFirstViewModel;
-import com.codegile.paul.databindingpoc.ui.main.views.second.MainActivitySecondView;
 import com.codegile.paul.databindingpoc.ui.main.views.second.MainActivitySecondViewModel;
-import com.codegile.paul.databindingpoc.ui.main.views.shapes.MainActivityShapesView;
 import com.codegile.paul.databindingpoc.ui.main.views.shapes.MainActivityShapesViewModel;
-import com.codegile.paul.databindingpoc.ui.main.views.third.MainActivityThirdView;
 import com.codegile.paul.databindingpoc.ui.main.views.third.MainActivityThirdViewModel;
 import com.codegile.paul.databindingpoc.ui.secondary.SecondaryActivity;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * Created by Paul on 10-Feb-17.
@@ -32,36 +27,28 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     private MainActivityThirdViewModel mThirdViewModel;
     private MainActivityShapesViewModel mShapesViewModel;
 
-    @Bind(R.id.first_view)
-    MainActivityFirstView mFirstView;
-    @Bind(R.id.second_view)
-    MainActivitySecondView mSecondView;
-    @Bind(R.id.third_view)
-    MainActivityThirdView mThirdView;
-    @Bind(R.id.shapes_view)
-    MainActivityShapesView mShapesView;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main_activity);
-        ButterKnife.bind(this);
+        MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
 
         this.mFirstViewModel = new MainActivityFirstViewModel();
         this.mSecondViewModel = new MainActivitySecondViewModel();
         this.mSecondViewModel.setFirstViewModel(mFirstViewModel);
         this.mThirdViewModel = new MainActivityThirdViewModel();
         this.mShapesViewModel = new MainActivityShapesViewModel();
+
         this.mPresenter = new MainPresenter(this);
-        this.mFirstView.setViewModel(mFirstViewModel);
-        this.mFirstView.setActionHandler(mPresenter);
-        this.mSecondView.setViewModel(mSecondViewModel);
-        this.mSecondView.setActionHandler(mPresenter);
-        this.mThirdView.setViewModel(mThirdViewModel);
-        this.mThirdView.setActionHandler(mPresenter);
-        this.mShapesView.setViewModel(mShapesViewModel);
-        this.mShapesView.setActionHandler(mPresenter);
+
+        binding.firstView.setViewModel(mFirstViewModel);
+        binding.firstView.setActionHandler(mPresenter);
+        binding.secondView.setViewModel(mSecondViewModel);
+        binding.secondView.setActionHandler(mPresenter);
+        binding.thirdView.setViewModel(mThirdViewModel);
+        binding.thirdView.setActionHandler(mPresenter);
+        binding.shapesView.setViewModel(mShapesViewModel);
+        binding.shapesView.setActionHandler(mPresenter);
     }
 
     @Override
